5a859fd54b44f22c1a9a00c47070eabd60936210
Kudriashova Anastasiia <kudriashova.ag@gmail.com>
2024-09-28 12:48:59 +0300

    Form antd

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,70 @@
+import { Button, Checkbox, Form, Input } from 'antd';
 import React from 'react';
-
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+      <div>
+        <h1>Login</h1>
+        <Form
+                name="basic"
+                layout='vertical'
+         
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     );
 }
 
