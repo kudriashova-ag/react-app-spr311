429c6f8b656c045674031c2e3034c9ba99bc1876
Kudriashova Anastasiia <kudriashova.ag@gmail.com>
2024-10-05 11:08:34 +0300

    useSearchParams

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,9 +1,10 @@
 // import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Link, Outlet, useLoaderData } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { Link, Outlet, useLoaderData, useSearchParams } from "react-router-dom";
+import "./Users.css";
+import { Col, Row } from "antd";
 const Users = () => {
-    /*const [users, setUsers] = useState([]);
+  /*const [users, setUsers] = useState([]);
 
     useEffect(() => {
         getUsers();
@@ -13,21 +14,45 @@ const Users = () => {
         const response = await axios.get('https://jsonplaceholder.typicode.com/users');
         setUsers(response.data);
     }*/
-    
-    const users = useLoaderData();
 
-    return (
-        <div>
-            <h1>Users</h1>
-            <ol>
-                {users.map(user => (
-                    <li key={user.id}> <Link to={`/users/${user.id}`}> {user.name} </Link></li>
-                ))}
-            </ol>
+  const users = useLoaderData();
+  const [search, setSearch] = useSearchParams();
+  const [textSearch, setTextSearch] = useState(search.get("q") || "");
 
-            <Outlet />
-        </div>
-    );
-}
+  const searchHandler = (e) => {
+    setTextSearch(e.target.value);
+    setSearch({ q: e.target.value });
+  };
+
+
+  const filterUsers = (user) => {
+    return user.name.toLowerCase().includes(textSearch.toLowerCase());
+  };
+  return (
+    <div>
+      <h1>Users</h1>
+      <Row>
+        <Col span={6}>
+          <div>
+            <input type="text" value={textSearch} onChange={searchHandler} />
+          </div>
+
+          <ol>
+            {users.filter(filterUsers).map((user) => (
+              <li key={user.id}>
+                {" "}
+                <Link to={`/users/${user.id}`}> {user.name} </Link>
+              </li>
+            ))}
+          </ol>
+        </Col>
+
+        <Col span={18}>
+          <Outlet />
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default Users;
